cd3256c556d4bcfff308965c0a667882fded80e3
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-08-26 14:41:53 +0300

    making custom module

--- web/modules/custom/first_module/src/Controller/FirstController.php
+++ web/modules/custom/first_module/src/Controller/FirstController.php
@@ -5,9 +5,9 @@ namespace Drupal\first_module\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
 
-class FirstController extends COntrollerBase {
+class FirstController extends ControllerBase {
     public function first () {
-        return new Response("Hello- My First Drupal Module");
+        return new Response("Hello - My First Drupal Module");
     }
 }
 
