3b6a263218cbc87d661a39825f6b6eeef2d8acb9
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-09-09 09:58:27 +0300

    add: practice task 2 completed

--- web/themes/custom/react_hello/js/src/index.jsx
+++ web/themes/custom/react_hello/js/src/index.jsx
@@ -1,6 +1,12 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
  
-const root = ReactDOM.createRoot(document.getElementById("react_app"));
-root.render(<h1>Hello there- world!</h1>);
+// const root = ReactDOM.createRoot(document.getElementById("react_app"));
+// root.render(<h1>Hello there- world!</h1>);
+ 
+
+const root = createRoot(document.getElementById("react_app"));
+root.render(<h1>Hello world I am react_in_Drupal</h1>);
  
